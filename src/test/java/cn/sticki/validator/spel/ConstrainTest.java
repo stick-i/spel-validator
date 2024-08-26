99bde40b9329aba4f7729f79ab97ca226d8ec592
阿杆 <sticki@qq.com>
2024-08-26 16:18:42 +0800

    test: 调整测试用例的断言时机，尽量让所有用例都执行完后再进行断言

--- src/test/java/cn/sticki/validator/spel/ConstrainTest.java
+++ src/test/java/cn/sticki/validator/spel/ConstrainTest.java
@@ -26,9 +26,9 @@ public class ConstrainTest {
     @Test
     void testSpelAssert() {
         boolean verified = ValidateUtil.checkConstraintResult(SpelAssertTestBean.paramTestCase());
-        Assertions.assertTrue(verified);
-
         boolean emptyTest = ValidateUtil.checkConstraintResult(SpelAssertTestBean.emptyTestCase());
+
+        Assertions.assertTrue(verified);
         Assertions.assertTrue(emptyTest);
     }
 
@@ -41,51 +41,49 @@ public class ConstrainTest {
     @Test
     void testSpelNotEmpty() {
         boolean verifiedParam = ValidateUtil.checkConstraintResult(SpelNotEmptyTestBean.paramTestCase());
-        Assertions.assertTrue(verifiedParam, "spelNotEmpty param test failed");
-
         boolean verifiedType = ValidateUtil.checkConstraintResult(SpelNotEmptyTestBean.typeTestCase());
+
+        Assertions.assertTrue(verifiedParam, "spelNotEmpty param test failed");
         Assertions.assertTrue(verifiedType, "spelNotEmpty type test failed");
     }
 
     @Test
     void testSpelNotNull() {
         boolean paramTest = ValidateUtil.checkConstraintResult(SpelNotNullTestBean.paramTestCase());
-        Assertions.assertTrue(paramTest, "spelNotNull param test failed");
-
         boolean typeTest = ValidateUtil.checkConstraintResult(SpelNotNullTestBean.typeTestCase());
-        Assertions.assertTrue(typeTest, "spelNotNull type test failed");
-
         boolean repeatableTest = ValidateUtil.checkConstraintResult(SpelNotNullTestBean.repeatableTestCase());
+
+        Assertions.assertTrue(paramTest, "spelNotNull param test failed");
+        Assertions.assertTrue(typeTest, "spelNotNull type test failed");
         Assertions.assertTrue(repeatableTest, "spelNotNull repeatable test failed");
     }
 
     @Test
     void testSpelNull() {
         boolean paramTest = ValidateUtil.checkConstraintResult(SpelNullTestBean.paramTestCase());
-        Assertions.assertTrue(paramTest, "spelNull param test failed");
-
         boolean typeTest = ValidateUtil.checkConstraintResult(SpelNullTestBean.typeTestCase());
-        Assertions.assertTrue(typeTest, "spelNull type test failed");
-
         boolean repeatableTest = ValidateUtil.checkConstraintResult(SpelNullTestBean.repeatableTestCase());
+
+        Assertions.assertTrue(paramTest, "spelNull param test failed");
+        Assertions.assertTrue(typeTest, "spelNull type test failed");
         Assertions.assertTrue(repeatableTest, "spelNull repeatable test failed");
     }
 
     @Test
     void testSpelSize() {
         boolean paramTest = ValidateUtil.checkConstraintResult(SpelSizeTestBean.paramTestCase());
-        Assertions.assertTrue(paramTest, "spelSize param test failed");
-
         boolean repeatableTest = ValidateUtil.checkConstraintResult(SpelSizeTestBean.repeatableTestCase());
+
+        Assertions.assertTrue(paramTest, "spelSize param test failed");
         Assertions.assertTrue(repeatableTest, "spelSize repeatable test failed");
     }
 
     @Test
     void testSpelMin() {
         boolean paramTest = ValidateUtil.checkConstraintResult(SpelMinTestBean.paramTestCase());
-        Assertions.assertTrue(paramTest, "spelMin param test failed");
-
         boolean repeatableTest = ValidateUtil.checkConstraintResult(SpelMinTestBean.repeatableTestCase());
+
+        Assertions.assertTrue(paramTest, "spelMin param test failed");
         Assertions.assertTrue(repeatableTest, "spelMin repeatable test failed");
     }
 }
