5d295a437800ee19982883bec46d808d5d721c5e
oddfar <oddfar@163.com>
2024-09-06 22:11:06 +0800

    feat: 封装数值比较工具，以此工具比较

--- src/main/java/cn/sticki/validator/spel/constraintvalidator/SpelMinValidator.java
+++ src/main/java/cn/sticki/validator/spel/constraintvalidator/SpelMinValidator.java
@@ -5,10 +5,9 @@ import cn.sticki.validator.spel.constrain.SpelMin;
 import cn.sticki.validator.spel.exception.SpelParserException;
 import cn.sticki.validator.spel.parse.SpelParser;
 import cn.sticki.validator.spel.result.FieldValidResult;
-import cn.sticki.validator.spel.util.BigDecimalUtil;
+import cn.sticki.validator.spel.util.NumberComparatorUtil;
 
 import java.lang.reflect.Field;
-import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -29,17 +28,13 @@ public class SpelMinValidator implements SpelConstraintValidator<SpelMin> {
         if (fieldValue == null) {
             return FieldValidResult.success();
         }
-        BigDecimal fieldValueBigDecimal = BigDecimalUtil.valueOf(fieldValue);
-
         // 计算表达式的值
         Object minValue = SpelParser.parse(spelMin.value(), obj);
         if (!(minValue instanceof Number) && !(minValue instanceof CharSequence)) {
             throw new SpelParserException("Expression [" + spelMin.value() + "] calculate result must be Number or CharSequence.");
         }
-        BigDecimal minValueBigDecimal = BigDecimalUtil.valueOf(minValue);
-
-        // 比较大小
-        if (fieldValueBigDecimal.compareTo(minValueBigDecimal) < 0) {
+        // 比较大小，其中一个是Not-a-Number (NaN）默认失败
+        if (NumberComparatorUtil.compare(fieldValue, minValue, NumberComparatorUtil.GREATER_THAN) < 0) {
             // 构建错误信息
             String message = spelMin.message();
             message = message.replace("{value}", String.valueOf(minValue));
